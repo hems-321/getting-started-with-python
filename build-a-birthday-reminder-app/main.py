a95c0e6012b7dfeb6f2ee701737090609acb5151
hems-321 <hmlt.pndy@gmail.com>
2025-01-20 21:03:36 +0530

    Add birthday add

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,6 @@
 user_input = input("Which birthday are you looking for? ")
+family_member_birthday = {"Hems": "25th December", "Harain": "22nd Nov", "Noya": "24th March", "Vasud": "4th April"}
+if user_input in family_member_birthday:
+    print(family_member_birthday[user_input])
+else:
+    print(f"{user_input} not found in your app")
