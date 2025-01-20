7592bf1f858d4dad485962780097683c1b0e88e7
hems-321 <hmlt.pndy@gmail.com>
2025-01-20 20:34:57 +0530

    Test arithmatic operator

---
+++ math-can-be-fun-with-python/main.py
@@ -0,0 +1,9 @@
+print(63-7)
+print(63+4)
+print(60*2)
+print(60/3)
+
+invested_value = 50000
+matured_value = 120000
+profit = matured_value - invested_value
+print(f"Profit on investment of {invested_value} is {profit}")
