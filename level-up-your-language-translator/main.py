fcb111b41b3df5a481c73dac2130d0f03b65415b
hems-321 <hmlt.pndy@gmail.com>
2025-01-22 21:15:59 +0530

    Add more practice code

--- level-up-your-language-translator/main.py
+++ level-up-your-language-translator/main.py
@@ -1,5 +1,14 @@
 import csv
 
+
+def intro():
+  print('Welcome to the Spanish and French translator app.\nPlease enter an English word and hit Enter.')
+  print('\nType "done" at any time to exit.')
+
+
+def exit_translator():
+  print('\nThanks for using the translator app. Have a great day!')
+
 translations = {}
 
 with open("level-up-your-language-translator/translations.csv", "r") as words:
@@ -22,6 +31,8 @@ while not done:
     done = True
   elif word in translations:
     print(translations[word])
+    print(f'\nSPANISH: {translations[word][0]}')
+    print(f'\nFRENCH: {translations[word][1]}')
   else:
     print("Translation is not known")
 
