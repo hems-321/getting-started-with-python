043228d324f8bf6038b2ffd995f1e0ea3d9e4233
hems-321 <hmlt.pndy@gmail.com>
2025-01-22 11:42:26 +0530

    Add more practice code

--- build-a-pet-shop-with-a-python-class/main.py
+++ build-a-pet-shop-with-a-python-class/main.py
@@ -1,6 +1,20 @@
 class Pet:
-  def __init__(self, name, species, breed, age, weight):
+    def __init__(self, name, species, breed, age, weight):
+        self.name = name
+        self.spices = species
+        self.breed = breed
+        self.age = age
+        self.weight = weight
+
+    def retrieve_pet(self):
+        print(f"Name: {self.name}")
+        print(f"spices: {self.spices}")
+        print(f"Breed: {self.breed}")
+        print(f"Age: {self.age}")
+        print(f"Weight: {self.weight}")
 
 
-my_pet = Pet("Max","Dog","Boston Terrier",3,10)
+my_pet = Pet("Max", "Dog", "Boston Terrier", 3, 10)
+
+print(my_pet.retrieve_pet())
 
